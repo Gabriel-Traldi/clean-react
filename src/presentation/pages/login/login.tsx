80991b3fefdd9510ea158233910132039ade0831
Gabriel Bruno Traldi <traldi13@hotmail.com>
2022-05-19 16:51:07 -0300

    refactor: move header and footer

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,17 +1,15 @@
 import React from 'react'
 
 import Spinner from '@/presentation/components/spinner/spinner'
-import Logo from '@/presentation/components/logo/logo'
+import Header from '@/presentation/components/login-header/login-header'
+import Footer from '@/presentation/components/footer/footer'
 
 import Styles from './login-styles.scss'
 
 const Login: React.FC = () => {
   return (
     <div className={Styles.login}>
-      <header className={Styles.header}>
-        <Logo />
-        <h1>4DEV - Enquetes para Programadores</h1>
-      </header>
+      <Header />
       <form className={Styles.form}>
         <h2>Login</h2>
         <div className={Styles.inputWrap}>
@@ -26,11 +24,11 @@ const Login: React.FC = () => {
         <span role="link" className={Styles.link}>Criar conta</span>
 
         <div className={Styles.errorWrap}>
-          <Spinner className={Styles.spinner}/>
+          <Spinner className={Styles.spinner} />
           <span className={Styles.error}>Erro</span>
         </div>
       </form>
-      <footer className={Styles.footer} />
+      <Footer />
     </div>
   )
 }
