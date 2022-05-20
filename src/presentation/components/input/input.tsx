08ed185d927a27a51d16cc6dad5473df5923af9a
Gabriel Bruno Traldi <traldi13@hotmail.com>
2022-05-20 14:37:03 -0300

    feat: remove autocomplete input on chrome

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -4,10 +4,10 @@ import Styles from './input-styles.scss'
 
 interface Props extends React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> { }
 
-const Input: React.FC<Props> = ({ ...rest }) => {
+const Input: React.FC<Props> = ({ autoComplete, ...rest }) => {
   return (
     <div className={Styles.inputWrap}>
-      <input {...rest} />
+      <input {...rest} autoComplete={autoComplete || 'off'} />
       <span className={Styles.status}>X</span>
     </div>
   )
