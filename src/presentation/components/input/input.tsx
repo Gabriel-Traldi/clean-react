e356722792e901a249bf0a63d1536fdbd58a5184
Gabriel Bruno Traldi <traldi13@hotmail.com>
2022-05-27 18:04:16 -0300

    feat: ensure login start with inputs with required state

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -1,14 +1,27 @@
-import React, { memo } from 'react'
+import React, { memo, useContext } from 'react'
+
+import Context from '@/presentation/contexts/form/form-context'
 
 import Styles from './input-styles.scss'
 
 interface Props extends React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> { }
 
 const Input: React.FC<Props> = ({ autoComplete, ...rest }) => {
+  const { errorState } = useContext(Context)
+  const error = errorState[rest.name]
+
+  const getStatus = (): string => {
+    return 'X'
+  }
+
+  const getTitle = (): string => {
+    return error
+  }
+
   return (
     <div className={Styles.inputWrap}>
       <input {...rest} autoComplete={autoComplete || 'off'} />
-      <span className={Styles.status}>X</span>
+      <span data-testid={`${rest.name}-status`} title={getTitle()} className={Styles.status}>{getStatus()}</span>
     </div>
   )
 }
--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -6,15 +6,15 @@ import Context from '@/presentation/contexts/form/form-context'
 
 import Styles from './login-styles.scss'
 
-type StateProps = {
-  isLoading: boolean
-  errorMessage: string
-}
-
 const Login: React.FC = () => {
-  const [state] = useState<StateProps>({
-    isLoading: false,
-    errorMessage: ''
+  const [state] = useState({
+    isLoading: false
+  })
+
+  const [errorState] = useState({
+    email: 'Campo obrigatório',
+    password: 'Campo obrigatório',
+    main: ''
   })
 
   return (
@@ -22,7 +22,7 @@ const Login: React.FC = () => {
 
       <LoginHeader />
 
-      <Context.Provider value={state}>
+      <Context.Provider value={{ state, errorState }}>
         <form className={Styles.form}>
           <h2>Login</h2>
 
