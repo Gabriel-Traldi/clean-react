165818927eb1dee48547351fd61ffb5e12fea4fc
Gabriel Bruno Traldi <traldi13@hotmail.com>
2022-05-27 18:51:52 -0300

    feat: ensure login calls validation with correct password

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -25,6 +25,10 @@ const Login: React.FC<Props> = ({ validation }) => {
     validation.validate({ email: state.email })
   }, [state.email])
 
+  useEffect(() => {
+    validation.validate({ password: state.password })
+  }, [state.password])
+
   return (
     <div className={Styles.login}>
 
