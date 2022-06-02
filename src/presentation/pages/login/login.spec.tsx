14750f2b98466e2e8d647b1439d8eda9b3eecfae
Gabriel Bruno Traldi <traldi13@hotmail.com>
2022-06-02 15:27:58 -0300

    test: ensure Login navigates to signup correctly

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import { Router } from 'react-router-dom'
+import { createMemoryHistory } from 'history'
+
 import { cleanup, fireEvent, render, RenderResult, waitFor, waitForElementToBeRemoved } from '@testing-library/react'
 import faker from '@faker-js/faker'
 
 import 'jest-localstorage-mock'
 
 import { ValidationStub, AuthenticationSpy } from '@/presentation/test'
+import { InvalidCredentialsError } from '@/domain/errors'
 
 import Login from './login'
-import { InvalidCredentialsError } from '@/domain/errors'
 
 type SutTypes = {
   sut: RenderResult
@@ -18,11 +21,16 @@ type SutParams = {
   validationError: string
 }
 
+const history = createMemoryHistory()
 const makeSut = (params?: SutParams): SutTypes => {
   const validationStub = new ValidationStub()
   const authenticationSpy = new AuthenticationSpy()
   validationStub.errorMessage = params?.validationError
-  const sut = render(<Login validation={validationStub} authentication={authenticationSpy} />)
+  const sut = render(
+    <Router location={history.location} navigator={history}>
+      <Login validation={validationStub} authentication={authenticationSpy} />
+    </Router>
+  )
   return {
     sut,
     authenticationSpy
@@ -154,5 +162,13 @@ describe('Login Component', () => {
     simulateValidSubmit(sut)
     await waitFor(() => sut.getByTestId('form'))
     expect(localStorage.setItem).toHaveBeenCalledWith('accessToken', authenticationSpy.account.accessToken)
+  })
+
+  test('Should go to signup page', async () => {
+    const { sut } = makeSut()
+    const register = sut.getByText(/criar conta/i)
+    fireEvent.click(register)
+    expect(history.index).toBeGreaterThan(0)
+    expect(history.location.pathname).toBe('/signup')
   })
 })
