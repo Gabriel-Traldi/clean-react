18428a654e0a5a34a40c7286c137e9865047c753
Gabriel Bruno Traldi <traldi13@hotmail.com>
2022-06-06 15:18:51 -0300

    test: ensure RequiredFieldValidation returns falsy if field is not empty

--- src/validation/required-field/required-field-validation.spec.ts
+++ src/validation/required-field/required-field-validation.spec.ts
@@ -1,4 +1,5 @@
 import { RequiredFieldError } from '@/validation/errors'
+import faker from '@faker-js/faker'
 
 import { RequiredFieldValidation } from './required-field.validation'
 
@@ -7,5 +8,11 @@ describe('RequiredFieldValidation', () => {
     const sut = new RequiredFieldValidation('email')
     const error = sut.validate('')
     expect(error).toEqual(new RequiredFieldError())
+  })
+
+  test('Should return falsy if field is not empty', () => {
+    const sut = new RequiredFieldValidation(faker.internet.email())
+    const error = sut.validate(faker.random.word())
+    expect(error).toBeFalsy()
   })
 })
