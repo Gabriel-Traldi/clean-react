647cd012a9cbd8af0c5b43d601d2dc01293e84d2
Gabriel Bruno Traldi <traldi13@hotmail.com>
2022-06-15 15:51:36 -0300

    feat: ensure SignUp change passwordConfirmation satus to error if validation fails

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -20,7 +20,8 @@ const SignUp: React.FC<Props> = ({ validation }) => {
     emailError: '',
     password: '',
     passwordError: '',
-    passwordConfirmationError: 'Campo obrigatório',
+    passwordConfirmation: '',
+    passwordConfirmationError: '',
     mainError: ''
   })
 
@@ -29,9 +30,10 @@ const SignUp: React.FC<Props> = ({ validation }) => {
       ...state,
       nameError: validation.validate('name', state.name),
       emailError: validation.validate('email', state.email),
-      passwordError: validation.validate('password', state.password)
+      passwordError: validation.validate('password', state.password),
+      passwordConfirmationError: validation.validate('passwordConfirmation', state.passwordConfirmation)
     }))
-  }, [state.name, state.email, state.password])
+  }, [state.name, state.email, state.password, state.passwordConfirmation])
 
   return (
     <div className={Styles.signup}>
