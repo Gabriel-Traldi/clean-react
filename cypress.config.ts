5183c09aceffb745ea01febf322ee5be74eee63b
Gabriel Bruno Traldi <traldi13@hotmail.com>
2022-06-24 15:00:01 -0300

    chore: add cypress command

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:8080',
     fixturesFolder: false,
-    supportFile: false,
+    supportFile: "src/main/test/cypress/support/index.js",
     specPattern: 'src/main/test/cypress/integration/**/*.{ts,tsx}',
     setupNodeEvents(on, config) {
       // implement node event listeners here
