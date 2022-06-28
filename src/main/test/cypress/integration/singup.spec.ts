5ec6b6632e210fa69ce29165baf5bab1e435ba6c
Gabriel Bruno Traldi <traldi13@hotmail.com>
2022-06-28 14:25:06 -0300

    test: esnure SignUp prevent submit if form is invalid

--- src/main/test/cypress/integration/singup.spec.ts
+++ src/main/test/cypress/integration/singup.spec.ts
@@ -110,4 +110,10 @@ describe('SignUp', () => {
     cy.getByTestId('submit').click().click()
     FormHelper.testHttpCallsCount(1)
   })
+
+  it('Should not call submit if form is invalid', () => {
+    Http.mockOk()
+    cy.getByTestId('email').focus().type(faker.internet.email()).type('{enter}')
+    FormHelper.testHttpCallsCount(0)
+  })
 })
